dc3e02172f6caeccf81df5a11edaebb54509113c
jayas <jayas@DESKTOP-M7R3V5E>
2022-03-10 17:45:23 +0530

    TestD

--- MavenTest/src/test/java/testDemo/Google.java
+++ MavenTest/src/test/java/testDemo/Google.java
@@ -1,15 +1,13 @@
 package testDemo;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+
 
 public class Google 
 {
 	public static void main(String[] sargs)
 	{
-		System.setProperty("webdriver.chrome.driver","F:\\Software_Sele\\chromedriver_win32\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
-		driver.get("https://www.google.co.in/");
+
 		System.out.println("open google");
 		
 	}
